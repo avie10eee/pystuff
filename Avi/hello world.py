23c0f78c6462db806e3d262211c832c1cf5ae96c
SandSlide <105246539+avie10eee@users.noreply.github.com>
2022-09-26 12:49:21 -0400

    Update hello world.py

--- Avi/hello world.py
+++ Avi/hello world.py
@@ -1,21 +1,4 @@
 
-#I ate pizza for dinner yesterday
-##print ("Hello")
-##print ("I am Avi Geller.")
-##print ("I like dogs, video games, Rollerblading and snow.")
-##print ("I am twelve years old.")
-##print ("I have a brother.")
-#His name is Itai, he is very annoying.
-##print ("I have three dogs.")
-##print ("My dogs names are Ziggy, Reese, Fonz.")
-#Ziggy is a medium sized black lab pitbull mix (we think). He has black fur and is five yrs old.
-##print ("I have four very small fish.")
-#Their names are matt, fiesta, pekosay and unnamed fish.
-##print ("I am a very beginner coder.")
-#I have tried to learn many coding languages.
-##print ("I have tried learning python before.")
-#I am also learning bash.
-#I had a bagel for breakfast.
 
 
 
